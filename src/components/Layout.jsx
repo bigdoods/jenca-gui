08f93ce0120d244b99515c8316144c2eeb6f959f
Kai Davenport <kaiyadavenport@gmail.com>
2016-01-31 18:45:51 +0000

    split apart the Layout component into various parts

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,7 @@
 import React, { Component, PropTypes } from 'react'
+import Header from './Header'
+import Menubar from './Menubar'
+import ContentPane from './ContentPane'
 
 class Layout extends Component {
   constructor(props) {
@@ -10,34 +13,9 @@ class Layout extends Component {
     return (
       <div className="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
                 mdl-layout--fixed-header">
-        <header className="mdl-layout__header">
-          <div className="mdl-layout__header-row">
-            <div className="mdl-layout-spacer"></div>
-            <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable
-                        mdl-textfield--floating-label mdl-textfield--align-right">
-              <label className="mdl-button mdl-js-button mdl-button--icon"
-                     htmlFor="fixed-header-drawer-exp">
-                <i className="material-icons">search</i>
-              </label>
-              <div className="mdl-textfield__expandable-holder">
-                <input className="mdl-textfield__input" type="text" name="sample"
-                       id="fixed-header-drawer-exp" />
-              </div>
-            </div>
-          </div>
-        </header>
-        <div className="mdl-layout__drawer">
-          <span className="mdl-layout-title">Title</span>
-          <nav className="mdl-navigation">
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
-          </nav>
-        </div>
-        <main className="mdl-layout__content">
-          <div className="page-content">Content here</div>
-        </main>
+        <Header />
+        <Menubar />
+        <ContentPane />
       </div>
     )
   }
