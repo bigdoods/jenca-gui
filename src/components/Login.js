4751ed4d614bfc61d32f980cc54bde9f5d0eadb5
Kai Davenport <kaiyadavenport@gmail.com>
2016-03-15 23:05:25 +0000

    remove bad form

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,13 +5,7 @@ export default function Bar() {
 	<div>
   	This is the login form!
   		<div class="login">
-	  		<form action="#">
-			  <div class="mdl-textfield mdl-js-textfield">
-			    <input class="mdl-textfield__input" type="text" id="sample1">
-			    <label class="mdl-textfield__label" for="sample1">Text...</label>
-			  </div>
-			</form>
-  			<footer><a href="#0">Create an account</a></footer>
+	  		
   		</div>
   	</div>
   )
