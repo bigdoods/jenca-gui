8c9bdf347a9c8d5b39deb6a8cf30514d4a47a0a2
Kai Davenport <kaiyadavenport@gmail.com>
2016-03-16 13:17:42 +0000

    center align

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,27 @@
 import React from 'react'
+import { Grid, Cell } from 'react-mdl';
+
 import CredentialsForm from './CredentialsForm'
 
 export default function LoginForm({loginMode, handleSwitch, handleRegister, handleLogin}) {
 
+  var form = loginMode == 'login' ? 
+    <CredentialsForm title="Login" handleSubmit={handleLogin} /> :
+    <CredentialsForm title="Register" handleSubmit={handleRegister} />
+
 
   return (
-  	<div className="mdl-layout mdl-js-layout mdl-color--white-100">
+  	<div className="mdl-layout mdl-js-layout mdl-color--white-100" style={{textAlign:'center'}}>
       <main className="mdl-layout__content">
 
-        <div className="mdl-grid">
-          <div className="mdl-cell mdl-cell--4-col">
-          
-              <CredentialsForm title="Login" handleSubmit={handleLogin} />
-
-              <CredentialsForm title="Register" handleSubmit={handleRegister} />
-          
-          </div>
-        </div>
+        <Grid>
+          <Cell col={12} align="middle">
+            <div style={{textAlign:'center'}}>
+              {form}
+            </div>
+          </Cell>
+        </Grid>
+    
         
       </main>
     </div>
